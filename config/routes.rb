1a70c7cfe71b1d5be22d437b365f04d7f12d48db
Waldemar C. Arbirk <waldemar.arbirk@gmail.com>
2015-12-01 12:39:26 +0000

    Added signup and signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'            # Creates root_path 
   get "about" => "pages#about" # Creates about_path
   # The priority is based upon order of creation: first created -> highest priority.
