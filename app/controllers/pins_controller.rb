3a2294212ae30b69cecbeaa13b2cd9354e95294b
Waldemar C. Arbirk <waldemar.arbirk@gmail.com>
2015-12-06 19:54:18 +0000

    Add pins/users association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,11 +1,13 @@
 class PinsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
   def index
     @pins = Pin.all
   end
 
-  def show
+   def show
   end
 
   def new
@@ -17,26 +19,24 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
-      render :new
+      render action: 'new'
     end
   end
 
   def update
-  
     if @pin.update(pin_params)
       redirect_to @pin, notice: 'Pin was successfully updated.'
     else
-      render :edit
+      render action: 'edit'
     end
   end
 
   def destroy
     @pin.destroy
-    redirect_to pins_url, notice: 'Pin was successfully destroyed.'
+    redirect_to pins_url
   end
 
   private
@@ -45,14 +45,13 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
-    end
-end
-
-Class PinsController < ActieRecord::Base
-    def show
-        @pin = Pin.find params[:id] #-> or Pin.first
     end
 end
